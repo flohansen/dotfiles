bf2c8d97aa8b3e69212d0c493d5b0f2b013d1b07
Florian Hansen <flhansen.93@gmail.com>
2024-08-11 17:38:34 +0200

    update nvim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,7 @@ vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
 vim.keymap.set('n', '<leader>af', vim.lsp.buf.format)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+vim.keymap.set('n', 'g?', vim.diagnostic.open_float)
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
