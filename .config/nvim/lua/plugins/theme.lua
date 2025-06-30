6d2383b488bad86d80f81afd4ebd1c2f38981689
Florian Hansen <flhansen.93@gmail.com>
2025-06-30 21:43:47 +0200

    update themes to catppuccin

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,113 +1,112 @@
 return {
   { 
-      "morhetz/gruvbox",
-      config = function()
-        vim.g.gruvbox_contrast_dark = 'soft'
-        vim.cmd [[ colorscheme gruvbox ]]
-        vim.cmd [[
-          highlight Normal guibg=none
-          highlight NormalNC guibg=none
-          highlight NonText guibg=none
-          highlight Normal ctermbg=none
-          highlight NonText ctermbg=none
-        ]]
-      end
+    "catppuccin/nvim",
+    config = function()
+      vim.cmd [[ colorscheme catppuccin-mocha ]]
+      vim.cmd [[
+        highlight Normal guibg=none
+        highlight NormalNC guibg=none
+        highlight NonText guibg=none
+        highlight Normal ctermbg=none
+        highlight NonText ctermbg=none
+      ]]
+    end
   },
   {
-      'neovim/nvim-lspconfig',
-      config = function()
-          require('lspconfig').gopls.setup({})
-          require('lspconfig').terraformls.setup({})
-          require('lspconfig').jsonnet_ls.setup({})
-          require('lspconfig').ts_ls.setup({})
-          require('lspconfig').buf_ls.setup({})
-      end,
+    'neovim/nvim-lspconfig',
+    config = function()
+      require('lspconfig').gopls.setup({})
+      require('lspconfig').terraformls.setup({})
+      require('lspconfig').jsonnet_ls.setup({})
+      require('lspconfig').ts_ls.setup({})
+      require('lspconfig').buf_ls.setup({})
+    end,
   },
   { 'hrsh7th/cmp-nvim-lsp' },
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require('lualine').setup()
+      require('lualine').setup()
     end
   },
   {
-      "kdheepak/lazygit.nvim",
-      lazy = true,
-      cmd = {
-          "LazyGit",
-          "LazyGitConfig",
-          "LazyGitCurrentFile",
-          "LazyGitFilter",
-          "LazyGitFilterCurrentFile",
-      },
-      dependencies = {
-          "nvim-lua/plenary.nvim",
-      },
-      keys = {
-          { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-      }
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
   },
   {
-      'hrsh7th/nvim-cmp',
-      config = function()
-          local cmp = require('cmp')
-          cmp.setup({
-            sources = { { name = 'nvim_lsp' } },
-            mapping = cmp.mapping.preset.insert({
-              ['<C-Space>'] = cmp.mapping.complete(),
-              ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            })
-          })
-      end,
+    'hrsh7th/nvim-cmp',
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup({
+        sources = { { name = 'nvim_lsp' } },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        })
+      })
+    end,
   },
   {
-      'nvim-telescope/telescope.nvim',
-      config = function()
-          require('telescope').setup()
-      end
+    'nvim-telescope/telescope.nvim',
+    config = function()
+      require('telescope').setup()
+    end
   },
   {
-      'nvim-tree/nvim-tree.lua',
-      dependencies = { 'nvim-tree/nvim-web-devicons' },
-      config = function()
-          require('nvim-tree').setup()
-      end
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('nvim-tree').setup()
+    end
   },
   {
-      'lukas-reineke/indent-blankline.nvim',
-      main = "ibl",
-      config = function()
-          require('ibl').setup()
-      end
+    'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
+    config = function()
+      require('ibl').setup()
+    end
   },
   {
-      'nvim-treesitter/nvim-treesitter',
-      config = function()
-          require('nvim-treesitter.configs').setup({
-            ensure_installed = { 'go', 'terraform', 'jsonnet', 'yaml', 'proto' },
-            highlight = { enable = true },
-          })
-      end
+    'nvim-treesitter/nvim-treesitter',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = { 'go', 'terraform', 'jsonnet', 'yaml', 'proto' },
+        highlight = { enable = true },
+      })
+    end
   },
   {
-      'williamboman/mason.nvim',
-      dependencies = { 'williamboman/mason-lspconfig.nvim' },
-      config = function()
-          require('mason').setup()
-          require('mason-lspconfig').setup({
-            ensure_installed = { 'gopls', 'terraformls', 'jsonnet_ls', 'ts_ls', 'buf_ls' },
-            automatic_installation = true,
-          })
-      end
+    'williamboman/mason.nvim',
+    dependencies = { 'williamboman/mason-lspconfig.nvim' },
+    config = function()
+      require('mason').setup()
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'gopls', 'terraformls', 'jsonnet_ls', 'ts_ls', 'buf_ls' },
+        automatic_installation = true,
+      })
+    end
   },
   {
-      'mfussenegger/nvim-dap',
-      dependencies = { 'rcarriga/nvim-dap-ui', 'nvim-neotest/nvim-nio', 'theHamsta/nvim-dap-virtual-text', 'ray-x/guihua.lua' },
-      config = function()
-          require('nvim-dap-virtual-text').setup()
-          require('guihua.maps').setup()
-      end
+    'mfussenegger/nvim-dap',
+    dependencies = { 'rcarriga/nvim-dap-ui', 'nvim-neotest/nvim-nio', 'theHamsta/nvim-dap-virtual-text', 'ray-x/guihua.lua' },
+    config = function()
+      require('nvim-dap-virtual-text').setup()
+      require('guihua.maps').setup()
+    end
   },
   {
     "ray-x/go.nvim",
@@ -122,7 +121,7 @@ return {
       vim.api.nvim_create_autocmd("BufWritePre", {
         pattern = "*.go",
         callback = function()
-        require('go.format').goimports()
+          require('go.format').goimports()
         end,
         group = format_sync_grp,
       })
